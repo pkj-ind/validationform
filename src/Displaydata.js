ee62f8dab89f96d5c73ae3770f726f1601532392
Pramod Jaiswal <pramodix@gmail.com>
2020-02-18 01:33:35 +0530

    dividng aadhar string equally and show

--- src/Displaydata.js
+++ src/Displaydata.js
@@ -1,15 +1,15 @@
 import React,{Fragment} from 'react'
 
 export const Displaydata = () => {
-    let aadhar="123456789512";
+    let aadhar="1234567895124521";
     let showval=aadhar.substr(0, aadhar.length/4) +" "+
     new  Array(aadhar.length+1 - aadhar.length/2 ).join('x') + " "+
     aadhar.substr(aadhar.length-aadhar.length/4, aadhar.length/4);
 
     return (
         <Fragment>
-            <h3>Aadhar: {aadhar}</h3>
-            <p>Masked Aadhar: {showval}</p>
+            <p><b>Aadhar:</b> {aadhar.match(/.{4}/g).join(" ")}</p>
+            <p><b>Masked Aadhar:</b> {showval}</p>
         <hr />
 
         </Fragment>
