7184e2531388c9a9cbbf1bf651088abbd55f4bac
Pramod Jaiswal <pramodix@gmail.com>
2020-03-01 10:14:34 +0530

    Aria-label Modal class updates

--- src/CallModal.js
+++ src/CallModal.js
@@ -4,48 +4,41 @@ import Modal from './Modal'
 
 class CallModal extends Component {
 
-    state = { show: false,
-    lastFocus: "" ,
-  modal:""};
-    
-    showModal = () => {
-      this.setState({ show: true });
-      this.setState({lastFocus : document.activeElement});
-      console.log(this.state.lastFocus)
-    };
+  state = {
+    show: false,
+    lastFocus: ""
+ 
+  };
+
+  showModal = () => {
+    this.setState({ show: true });
+    this.setState({ lastFocus: document.activeElement });
+   // this.setState({ modal: document.getElementById('modal-id') });
+    console.log(this.state)
+
+  };
+
+  hideModal = () => {
+    console.log("inside hidemodal", this.state.lastFocus)
+    this.setState({ show: false });
+    this.state.lastFocus.focus();
+  };
+
+  render() {
+
+
   
-    hideModal = () => {
-      console.log("inside hidemodal",this.state.lastFocus)
-      this.setState({ show: false });
-      this.state.lastFocus.focus();
-    };
-  
-    render() {
-
-      //var modal = document.getElementById('modal-id');
-
-
-      return (
-        <main>
-          <h1 className="prisha">React Modal</h1>
-          <Modal show={this.state.show} handleClose={this.hideModal}
-          aria={{
-            labelledby: "heading",
-            describedby: "full_description"
-          }}
-          id="modal-id" >
-            <h1 id="heading" className="title">Modal</h1>
-            <div id="full_description" className="description">
-            <p>Description goes here.</p>
-            </div>           
-          </Modal>
-          <button type="button" onClick={this.showModal}>
-            open
+    return (
+      <main>
+        <h1 className="prisha">React Modal</h1>
+        <Modal show={this.state.show} handleClose={this.hideModal} />
+        <button type="button" onClick={this.showModal}>
+          open
           </button>
-        </main>
-      );
-    }
-  
+      </main>
+    );
+  }
+
 }
 
 export default CallModal;
--- src/Modal.js
+++ src/Modal.js
@@ -1,34 +1,40 @@
-import React,{useRef} from 'react'
+import React, { useRef } from 'react'
 
-const Modal = ({ handleClose, show, children }) => {
+const Modal = ({ handleClose, show }) => {
   //  const showHideClassName = show ? "modal display-block" : "modal display-none";
-  const cardModal=useRef(null)
+  const cardModal = useRef(null)
   show && cardModal.current.focus()
 
   function onKeyPressed(e) {
     console.log(e.key);
-   if(e.keyCode===27){
-     console.log("Escaped pressed");
-     handleClose()
-   }
+    if (e.keyCode === 27) {
+      console.log("Escaped pressed");
+      handleClose()
+    }
   }
-    return (
-      <div className={show ? "modal display-block" : "modal display-none"}>
-        <section className="modal-main" role="main" tabIndex="0" 
-        ref={cardModal} onKeyDown={(e) => onKeyPressed(e)}>
-          <button type="button" 
-          class="btn-close" 
-          id="modal_close" 
-          aria-label="close" 
-          onClick={handleClose}>
-            X
+  return (
+    <div className={show ? "modal display-block" : "modal display-none"} 
+    role="dialog" id="modal_window"
+    aria-labelledby="modal_title">
+      <section className="modal-main"
+        ref={cardModal} onKeyDown={(e) => onKeyPressed(e)} >
+        <button type="button"
+          class="btn-close"
+          id="modal_close"
+          aria-label="close"
+          onClick={handleClose}
+          >
+          X
           </button>
-          {children}
-          <button onClick={handleClose} className="mdl_btn btn">close</button>
+          <h1 id="modal_title" className="title" >Modal</h1>
+          <div id="full_description" className="description" aria-describedby="full_description">
+            <p>Description goes here.</p>
+          </div>
+        <button onClick={handleClose} className="mdl_btn btn">close</button>
 
-        </section>
-      </div>
-    );
-  };
+      </section>
+    </div>
+  );
+};
 
-  export default Modal;
+export default Modal;
